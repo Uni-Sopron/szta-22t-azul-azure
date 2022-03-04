9a6583ced0e7626ebb4c26d1bf70c45f94f3115b
LoremIPsummer <goodiesoftofficial@gmail.com>
2022-03-04 02:51:01 +0000

    Deploying to gh-pages from @ Uni-Sopron/szta-22t-azul-azure@7e9415ea551d6767dc19ff69693576947a572a81 🚀

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"azul","c":"App","l":"App()","url":"%3Cinit%3E()"},{"p":"azul","c":"App","l":"getGreeting()"},{"p":"azul","c":"App","l":"main(String[])","url":"main(java.lang.String[])"}]
