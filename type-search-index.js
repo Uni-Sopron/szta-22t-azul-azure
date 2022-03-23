8ad246766af9e74b448c4aff35351fd002f30fa6
LoremIPsummer <goodiesoftofficial@gmail.com>
2022-03-23 15:18:00 +0000

    Deploying to gh-pages from @ Uni-Sopron/szta-22t-azul-azure@6cb0886ecf3785d1d02b37bed5e116fd5ff317a8 🚀

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"azul","l":"App"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"azul","l":"App"},{"p":"azul","l":"Color"},{"p":"azul","l":"Row"},{"p":"azul","l":"Tile"}]
