7933427f8066cde04594ce73439071204df130d8
Mate Szabolcs Kiss <goodiesoftofficial@gmail.com>
2022-04-08 02:55:26 +0200

    Integrate jacoco and pmd indicators

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,12 +51,15 @@ tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
+
 tasks.test {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
 }
+
+
 
 jacoco {
     toolVersion = "0.8.8"
