f87a4ec5cc95a77568df514edc1f1846ede05d05
Mate Szabolcs Kiss <goodiesoftofficial@gmail.com>
2022-04-09 16:24:27 +0200

    Aggregate docs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,38 @@ tasks.named<Test>("test") {
 }
 
 
+
+tasks.register<Copy>("collectJacoco") {
+    dependsOn("jacocoTestReport")
+    from(layout.buildDirectory.dir("reports/jacoco/test/html"))
+    include("*")
+    into(layout.buildDirectory.dir("doc/jacoco"))
+}
+tasks.register<Copy>("collectPMD") {
+    from(layout.buildDirectory.dir("reports/pmd"))
+    include("*")
+    into(layout.buildDirectory.dir("doc/pmd"))
+}
+tasks.register<Copy>("collectJavadoc") {
+    dependsOn("aggregateJavadoc")
+    from(layout.buildDirectory.dir("docs/aggregateJavadoc"))
+    include("*")
+    into(layout.buildDirectory.dir("doc/javadoc"))
+}
+
+tasks.build {
+    finalizedBy("collectPMD")
+    finalizedBy("collectJacoco")
+    finalizedBy("collectJavadoc")
+}
+
+
 tasks.test {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+    finalizedBy(tasks.aggregateJavadoc)
+   
 }
+
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
 }
@@ -81,3 +110,5 @@ pmd {
     ruleSets = listOf()
     ruleSetConfig = resources.text.fromFile("../ruleset.xml")
 }
+
+
